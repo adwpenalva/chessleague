4d98bdca8c08ebd4b37fbede786e2bf7b92696e7
Antonio Penalva <adwpenalva@gmail.com>
2021-02-12 22:54:01 -0300

    chrome tab icon and title added, minor css tweaks

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,6 +8,7 @@ export default function Home(props) {
         <div className="Home">
         <div className="Home-header">
         <img src={logo} className="Home-logo" alt="logo" />
+        <br></br>
         <p>
           SOON<sup>(TM)</sup>
         </p>
